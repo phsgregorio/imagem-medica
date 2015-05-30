cb8942e0ba336aaf87760bdfbd658df0136fb297
Pedro Gregório <pedro.temsenha@gmail.com>
2015-05-30 10:11:34 -0300

    Article Update

--- src/com/puc/commons/globals/Globals.java
+++ src/com/puc/commons/globals/Globals.java
@@ -5,8 +5,8 @@ import java.io.File;
 public class Globals {
 	
 	public static Integer MAX_RESULTS_SQL = 10;
-	public static String FILE_DIR = "C:/tmp/";
-	public static String PATIENT_FILE_DIR = "C:/tmp/paciente";
+	public static String FILE_DIR = "/home/diamedico/temp/";
+	public static String PATIENT_FILE_DIR = FILE_DIR + "paciente";
 	public static String FILE_SEPARATOR = File.separator;
 	public static String TMP_IMAGE = ".tmp.jpg";
 	public static String DCM_EXTENSION = ".dcm";
